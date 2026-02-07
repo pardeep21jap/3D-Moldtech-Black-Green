99781bf598913d1774a67c5133867efb4e346bcf
pardeep21jap <pardeep21jap@gmail.com>
2026-02-06 19:00:20 -0800

    update images

--- src/Components/Protostory.jsx
+++ src/Components/Protostory.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Story.css"
 import { Link } from "react-router-dom";
-import storyImg from "../assets/story.png";
+import storyImg from "../assets/story1.webp";
 function Story() {
 
   return (
@@ -13,15 +13,15 @@ function Story() {
               <div className="row story-content">
                 {/* Left Text */}
                 <div className="col-lg-8 story-text">
-                <h3 className='inner-heading'>What is low-volume Injection Molding</h3>
+                  <h3 className='inner-heading'>What is low-volume Injection Molding</h3>
 
 
                   <p className='allp story-desc'>
-                   Low-volume injection molding refers to creating a “low” number of parts using the injection molding services following the specifications and requests of the customer. Injection molding is an incredibly versatile process used to create complex parts by injecting plastic resin into metal molds. Injection molding provides unmatched part precision, consistency, and reliability to the consumer. 
+                    Low-volume injection molding refers to creating a “low” number of parts using the injection molding services following the specifications and requests of the customer. Injection molding is an incredibly versatile process used to create complex parts by injecting plastic resin into metal molds. Injection molding provides unmatched part precision, consistency, and reliability to the consumer.
                   </p>
 
                   <p className='allp story-desc'>
-                  While many companies may utilize injection molding for significant part quantities, injection molding can also be used to deliver cost-effective results on limited part runs in what is known as low-volume injection molding. At Seaway Plastics Engineering, low-volume injection molding applies to any part run between 100 and 10,000 pieces. However, our average production lot is 1,000 pieces and progresses upwards from there.
+                    While many companies may utilize injection molding for significant part quantities, injection molding can also be used to deliver cost-effective results on limited part runs in what is known as low-volume injection molding. At Seaway Plastics Engineering, low-volume injection molding applies to any part run between 100 and 10,000 pieces. However, our average production lot is 1,000 pieces and progresses upwards from there.
                   </p>
                 </div>
 
